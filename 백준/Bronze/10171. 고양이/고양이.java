ab3917657b91997de85fcd2ff705a0d92dde00ea
Seunggu <83169867+Seung-9@users.noreply.github.com>
2023-07-29 17:19:22 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void solution() {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(" + "__)|");
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
