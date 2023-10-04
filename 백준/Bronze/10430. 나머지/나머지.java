e0ffa78bd4b0f7cec4dc9df62017fe08cb92da41
Seunggu <83169867+Seung-9@users.noreply.github.com>
2023-10-04 18:32:41 +0900

    [Bronze V] Title: 나머지, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int A, B, C;
+
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+    }
+
+    public static void solution() {
+        StringBuilder sb = new StringBuilder();
+        sb.append((A+B)%C).append("\n").append(((A%C) + (B%C))%C).append("\n").append((A*B)%C).append("\n").append(((A%C) * (B%C))%C);
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+    }
+}
