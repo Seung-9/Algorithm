85926e0c3f921d3ee6c3019196567ee84f882af4
Seunggu <83169867+Seung-9@users.noreply.github.com>
2024-02-27 15:22:23 +0900

    [Silver III] Title: N과 M (5), Time: 348 ms, Memory: 28208 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,38 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    static int N, M;
-    static int[] arr, answer;
-    static StringBuilder sb = new StringBuilder();
+
+    public static StringBuilder sb = new StringBuilder();
+    public static int N, M;
+    public static int[] nums, arr;
+    public static boolean[] visited;
 
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N];
-        answer = new int[M];
+        nums = new int[M];
+        visited= new boolean[10000];
 
         st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < N; i++)  arr[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(arr);
+        arr = new int[N]; // 2번째 줄
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr); // 내림차순으로 정렬
     }
 
-    public static void solution(int k) {
-        if(k == M) {
-            for(int i = 0; i < M; i++)  sb.append(answer[i]).append(" ");
+    public static void rec_func(int k) {
+        if (k == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(nums[i]).append(" ");
+            }
             sb.append("\n");
         } else {
-            for(int i = 0; i < N; i++) {
-                boolean isUsed = false;
-                for(int j = 0; j < k; j++)  if(arr[i] == answer[j]) isUsed = true;
-                if(!isUsed) {
-                    answer[k] = arr[i];
-                    solution(k + 1);
+            for (int i = 0; i < N; i++) {
+                if (!visited[arr[i]]) {
+                    visited[arr[i]] = true;
+                    nums[k] = arr[i];
+                    rec_func(k + 1);
+                    visited[arr[i]] = false;
                 }
             }
         }
@@ -40,7 +48,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         input();
-        solution(0);
+        rec_func(0);
         System.out.println(sb);
     }
 }
