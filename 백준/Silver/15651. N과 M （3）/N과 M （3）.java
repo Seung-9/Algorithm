6069eaa43146b9b93c7f25428166e7a9a15e4701
Seunggu <83169867+Seung-9@users.noreply.github.com>
2024-02-27 14:26:03 +0900

    [Silver III] Title: N과 M (3), Time: 432 ms, Memory: 67788 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,36 +1,40 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
 import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
-    static StringBuilder sb = new StringBuilder();
+
+    public static StringBuilder sb = new StringBuilder();
+    public static int N, M;
+    public static int[] nums;
 
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[M];
+        nums = new int[M];
     }
 
-    public static void solution(int k) {
-        if(k == M) {
-            for(int i = 0; i < M; i++)  sb.append(arr[i]).append(" ");
+    public static void rec_func(int k) {
+        if (k == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(nums[i]).append(" ");
+            }
             sb.append("\n");
         } else {
-            for(int i = 1; i <= N; i++) {
-                arr[k] = i;
-                solution(k + 1);
+            for (int i = 1; i <= N; i++) {
+                nums[k] = i;
+                rec_func(k + 1);
             }
         }
     }
 
     public static void main(String[] args) throws IOException {
         input();
-        solution(0);
+        rec_func(0);
         System.out.println(sb);
     }
 }
