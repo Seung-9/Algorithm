6a56db6061a6ef69f4b2af396d928623cc5762b1
Seunggu <83169867+Seung-9@users.noreply.github.com>
2024-06-28 22:46:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17532 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void solution() {
+        Scanner sc = new Scanner(System.in);
+        int ch = sc.next().charAt(0);
+        System.out.println(ch);
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
