3ae6cdb7c53b44c632c4990ee220e397b594ef8d
Seunggu <83169867+Seung-9@users.noreply.github.com>
2025-02-13 18:42:22 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] emergency) { // [3, 76, 24]
+        int[] answer = new int[emergency.length];
+        for (int i = 0; i < emergency.length; i++) {
+            for (int j = 0; j < emergency.length; j++) {
+                if(emergency[i] < emergency[j]) {
+                    answer[i]++;
+                }
+            }
+            answer[i]++;
+        }
+        return answer;
+    }
+}
