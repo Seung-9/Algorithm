1d418075410befd13eeb4c2dbab2e590e827654e
Seunggu <83169867+Seung-9@users.noreply.github.com>
2025-02-19 20:10:19 +0900

    [Bronze III] Title: 약수 구하기, Time: 104 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -2,35 +2,35 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class Main {
 
-    static int N;
-    static int k;
+    static int N, K;
 
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         N = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
     }
 
-    public static int solution() {
-        int count = 0;
-        for(int i = 1; i <= N; i++) {
-            if(N % i == 0) {
-                count++;
-            }
-            if(count == k) {
-                return i;
+    public static void solution() {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                arr.add(i);
             }
         }
-        return 0;
+        if (arr.size() < K) {
+            System.out.print(0);
+        } else {
+            System.out.print(arr.get(K - 1));
+        }
     }
 
     public static void main(String[] args) throws IOException {
         input();
-        System.out.println(solution());
+        solution();
     }
 }
