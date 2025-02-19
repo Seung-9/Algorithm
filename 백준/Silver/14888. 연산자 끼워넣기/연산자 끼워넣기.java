a07605ec71e9de7333f9f5fab11f126276e4df62
Seunggu <83169867+Seung-9@users.noreply.github.com>
2025-02-19 22:50:39 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 16652 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,44 +1,44 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.lang.Math;
 
 public class Main {
 
     static int N;
-    static int min = Integer.MAX_VALUE;
-    static int max = Integer.MIN_VALUE;
+    static int[] num;
     static int[] operator = new int[4];
-    static int[] nums;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
 
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
-        nums = new int[N];
-
+        num = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(st.nextToken());
-
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
         st = new StringTokenizer(br.readLine(), " ");
-
-        for(int i = 0; i < 4; i++)  operator[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < 4; i++) {
+            operator[i] = Integer.parseInt(st.nextToken());
+        }
     }
 
-    public static void rec_func(int k, int value) {
-        if(k == N) {
+    public static void solution(int index, int value) {
+        if(index == N) {
             max = Math.max(max, value);
             min = Math.min(min, value);
         } else {
-            for(int i = 0; i < 4; i++) {
-                if(operator[i] > 0) {
+            for (int i = 0; i < 4; i++) {
+                if (operator[i] > 0) {
                     operator[i]--;
                     switch (i) {
-                        case 0 : rec_func(k + 1, value + nums[k]); break;
-                        case 1 : rec_func(k + 1, value - nums[k]); break;
-                        case 2 : rec_func(k + 1, value * nums[k]); break;
-                        case 3 : rec_func(k + 1, value / nums[k]); break;
+                        case 0: solution(index + 1, value + num[index]); break;
+                        case 1: solution(index + 1, value - num[index]); break;
+                        case 2: solution(index + 1, value * num[index]); break;
+                        case 3: solution(index + 1, value / num[index]); break;
                     }
                     operator[i]++;
                 }
@@ -48,7 +48,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         input();
-        rec_func(1, nums[0]);
+        solution(1, num[0]);
         System.out.println(max);
         System.out.println(min);
     }
